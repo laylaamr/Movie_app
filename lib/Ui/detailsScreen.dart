790305f4872c6878d4203cdcf4c07d5b30c83caa
laylaamr <laylamuhana981@gmail.com>
2025-02-06 19:45:43 +0200

    detail screen done

--- lib/Ui/detailsScreen.dart
+++ lib/Ui/detailsScreen.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
-class DetailScreen extends StatelessWidget {
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:untitled122222/Api/model.dart';
+
+import '../cubitFiles/favoriteCubit.dart';
+class MovieDetailScreen extends StatelessWidget {
+  final Movie movie;
+
+  const MovieDetailScreen({Key? key, required this.movie}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Screen',
-        style: TextStyle(fontSize: 24),
-      ),
+  Widget build(BuildContext context) {return
+    SafeArea(
+      child: Scaffold(
+        body:
+      SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+          Stack(
+            //  alignment: Alignment.bottomCenter,
+            children: [SizedBox(
+              child: Image.network(
+                movie.bigImage,
+                fit: BoxFit.contain,
+                width: double.infinity,
+              ),
+            ),
+              Positioned(
+                 top:  16,
+                  child: IconButton(onPressed: (){
+                    Navigator.pop(context);
+                  }, icon: Icon(Icons.arrow_back,
+                  size: 25,color: Color(0xFF64B5F6),)))
+            ]
+          ),
+          Padding(
+        padding: const EdgeInsets.fromLTRB(10, 8, 10, 5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(movie.title, style: TextStyle(fontSize: 20),
+                softWrap: true,),
+                Row(children: [
+                  Icon(Icons.star_rate_outlined, color: Color( 0xFF64B5F6),size: 28,),
+                  SizedBox(width: 5,),
+                  Text(movie.rating, style: TextStyle(fontSize: 20),),],),
+              ],
+            ),
+            Text("Genre : ${movie.genre}",style: TextStyle(fontSize: 15)),
+            SizedBox(height: 10,),
+            Text("${movie.description}", style: TextStyle(fontSize: 18),),
+            SizedBox(height: 10,),
+            ElevatedButton(onPressed: (){},  style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+              backgroundColor: const Color(0xFF0D47A1),
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+        child: const Center(
+          child: Text(
+            'Watch Trailer',
+            style: TextStyle(fontSize:20),
+          ),
+        ),),
+            SizedBox(width: 10,)
+          ],
+        ),
+          ),
+        
+        ],),
+      )
+        ,),
     );
   }
 }
--- lib/widgets/movieCard.dart
+++ lib/widgets/movieCard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:untitled122222/Api/model.dart';
 
+import '../Ui/detailsScreen.dart';
 import '../cubitFiles/cubitMovie.dart';
 import '../cubitFiles/favoriteCubit.dart';
 import '../cubitFiles/favotite state.dart';
@@ -50,23 +51,106 @@ class MovieList extends StatelessWidget {
       
           return InkWell(
             onTap: () {
-              // Navigate to the detail screen if needed
-            },
-            child: ListTile(
-              leading: Image.network(movie.image),
-              title: Text(movie.title),
-              subtitle: Text('Rank: ${movie.id}'),
-              trailing: IconButton(
-                icon: Icon(
-                  isFav ? Icons.favorite : Icons.favorite_border,
-                  color: isFav ? Colors.red : Colors.black,
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MovieDetailScreen(movie: movie),
                 ),
-                onPressed: () {
-                  // Toggle favorite
-                  context.read<FavoriteCubit>().toggleFavorite(movie);
-                },
-              ),
-            ),
+              );
+            },
+            child:
+              Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                height: 190,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.1),
+                      blurRadius: 6,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Row(
+children: [
+  AspectRatio(
+    aspectRatio: 1,
+    child: ClipRRect(
+      borderRadius: const BorderRadius.only(
+        topLeft: Radius.circular(12),
+        topRight: Radius.circular(12),
+      ),
+      child: Image.network(
+        movie.bigImage,
+        fit: BoxFit.contain,
+        width: double.infinity,
+      ),
+    ),
+  ),
+ Expanded(child: Padding(padding: EdgeInsets.symmetric(horizontal: 8),
+ child: Column(
+   crossAxisAlignment: CrossAxisAlignment.start,
+   children: [
+     Text(
+       movie.title,
+       style: const TextStyle(
+         fontSize: 18,
+         fontWeight: FontWeight.bold,
+         color: Colors.black,
+       ),
+       softWrap: true,
+     ),
+     SizedBox(height: 10,),
+     Text("Genre : ${movie.genre.toString()}",  style: const TextStyle(
+       fontSize: 15,
+
+       color: Colors.black,
+     ),
+       softWrap: true,),
+     SizedBox(height: 7,),
+     Row(
+       children: [
+         Text("Rank : ${movie.rank.toString()}",  style: const TextStyle(
+           fontSize: 15,
+
+           color: Colors.black,
+         ),
+           softWrap: true,),
+         SizedBox(width: 50,),
+         IconButton(
+           icon: Icon(
+             isFav ? Icons.favorite : Icons.favorite_border,
+             color: isFav ? Colors.red : Colors.black,
+           ),
+           onPressed: () {
+             context.read<FavoriteCubit>().toggleFavorite(movie);
+           },
+         ),
+       ],
+     ),
+
+ ],),
+ ))
+],
+
+                ),
+              )
+          //ListTile(
+            //   leading: Image.network(movie.bigImage),
+            //   title: Text(movie.title),
+            //   subtitle: Text('Rank: ${movie.id}'),
+            //   trailing: IconButton(
+            //     icon: Icon(
+            //       isFav ? Icons.favorite : Icons.favorite_border,
+            //       color: isFav ? Colors.red : Colors.black,
+            //     ),
+            //     onPressed: () {
+            //       context.read<FavoriteCubit>().toggleFavorite(movie);
+            //     },
+            //   ),
+            // ),
           );
         },
       ),
